6ae96ffedb82f01c3dab3f76841a10395a5595d1
Daniel Kulp <dkulp@apache.org>
2014-09-12 12:40:56 -0400

    Create an XJC plugin that can work around  https://java.net/jira/browse/JAXB-986

---
+++ bug986/src/main/java/org/apache/cxf/xjc/bug986/Bug986Plugin.java
@@ -0,0 +1,103 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.apache.cxf.xjc.bug986;
+
+
+import java.io.StringWriter;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import javax.xml.bind.annotation.XmlSchemaType;
+
+import org.xml.sax.ErrorHandler;
+
+import com.sun.codemodel.JAnnotationUse;
+import com.sun.codemodel.JAnnotationValue;
+import com.sun.codemodel.JFieldVar;
+import com.sun.codemodel.JFormatter;
+import com.sun.codemodel.JVar;
+import com.sun.tools.xjc.Options;
+import com.sun.tools.xjc.Plugin;
+import com.sun.tools.xjc.outline.ClassOutline;
+import com.sun.tools.xjc.outline.Outline;
+
+/**
+ * Modifies the JAXB code model to handle package naming that run into:
+ * https://jaxb.dev.java.net/issues/show_bug.cgi?id=671
+ */
+public class Bug986Plugin {
+    private static final Logger LOG = Logger.getLogger(Bug986Plugin.class.getName()); //NOPMD
+
+    final Plugin plugin;
+
+    public Bug986Plugin(Plugin p) {
+        plugin = p;
+    }
+    
+    
+    public String getOptionName() {
+        return "Xbug986";
+    }
+
+    public String getUsage() {
+        return "  -Xbug986             : Activate plugin remove XmlSchemaType(anySimpleType) from fields that shouldn't have it.";
+    }
+
+    public boolean run(Outline outline, Options opt, ErrorHandler errorHandler) {
+        // kind of a bogus thing to have to do to workaround bug:
+        // https://java.net/jira/browse/JAXB-986
+        LOG.info("Running Bug986Plugin plugin.");
+        for (ClassOutline classOutline : outline.getClasses()) {
+            Map<String, JFieldVar> fields = classOutline.implClass.fields();
+            for (JFieldVar field : fields.values()) {
+                Collection<JAnnotationUse> annotations = field.annotations();
+                List<JAnnotationUse> toRemove = new ArrayList<JAnnotationUse>();
+                for (JAnnotationUse j : annotations) {
+                    if (XmlSchemaType.class.getName().equals(j.getAnnotationClass().fullName())) {
+                        JAnnotationValue st = j.getAnnotationMembers().get("name");
+                        StringWriter sw = new StringWriter();
+                        st.generate(new JFormatter(sw));
+                        if (sw.toString().equals("\"anySimpleType\"")
+                            && field.type().fullName().startsWith("java.util.List")
+                            && !field.type().fullName().contains("<java.lang.String>")) {
+                            toRemove.add(j);
+                        }
+                    }
+                }
+                for (JAnnotationUse j : toRemove) {
+                    try {
+                        Field f = JVar.class.getDeclaredField("annotations");
+                        f.setAccessible(true);
+                        List<?> anns = (List<?>)f.get(field);
+                        anns.remove(j);
+                    } catch (Throwable t) {
+                        //ignore for now
+                        t.printStackTrace();
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
