4f2eb16080cf44b4740f798dc17032ac836dff28
Andriy Redko <drreta@gmail.com>
2023-05-30 07:49:29 -0400

    CXFXJC-44: cxf-xjc-dv generates compilation failure when XJC simple binding mode is used (#69)

--- dv/src/main/java/org/apache/cxf/xjc/dv/DefaultValuePlugin.java
+++ dv/src/main/java/org/apache/cxf/xjc/dv/DefaultValuePlugin.java
@@ -34,6 +34,7 @@ import org.xml.sax.ErrorHandler;
 
 import com.sun.codemodel.ClassType;
 import com.sun.codemodel.JBlock;
+import com.sun.codemodel.JClass;
 import com.sun.codemodel.JConditional;
 import com.sun.codemodel.JDefinedClass;
 import com.sun.codemodel.JDocComment;
@@ -202,9 +203,16 @@ public class DefaultValuePlugin {
                         || isElementRequired(particle))) {
                     String varName = f.getPropertyInfo().getName(false);
                     JFieldVar var = co.implClass.fields().get(varName);
-                    if (var != null 
-                        && !"jakarta.xml.ws.wsaddressing.W3CEndpointReference".equals(f.getRawType().fullName())) {
-                        var.init(JExpr._new(f.getRawType()));
+                    final JType rawType = f.getRawType();
+                    if (var != null && !"jakarta.xml.ws.wsaddressing.W3CEndpointReference".equals(rawType.fullName())) {
+                        if (rawType instanceof JClass) {
+                            final JClass jclazz = (JClass) rawType;
+                            if (!jclazz.isAbstract() && !jclazz.isInterface()) {
+                                var.init(JExpr._new(rawType));
+                            }
+                        } else {
+                            var.init(JExpr._new(rawType));
+                        }
                     }
                 }
 
