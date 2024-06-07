ca03e689dd11faf97709609c53e0b297e3b153a6
Marcel Hödl <marcel-555@hotmail.de>
2024-06-08 00:15:55 +0200

    CXFXJC-47: XJC DefaultValue plugin uses JAXBElement that does not have a default constructor (#129)
    * Added new Parameter to exclude specific classes from default value generation
    
    * CXFXJC-47: XJC DefaultValue plugin uses JAXBElement that does not have a default constructor
    
    ---------
    
    Co-authored-by: Marcel Hödl <marcel.hoedl@ofdka.bwl.de>
    Co-authored-by: Andriy Redko <drreta@gmail.com>

--- dv/src/main/java/org/apache/cxf/xjc/dv/DefaultValuePlugin.java
+++ dv/src/main/java/org/apache/cxf/xjc/dv/DefaultValuePlugin.java
@@ -21,7 +21,10 @@ package org.apache.cxf.xjc.dv;
 
 import java.io.IOException;
 import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -71,8 +74,17 @@ import jakarta.xml.bind.annotation.adapters.HexBinaryAdapter;
  * with their default value.
  */
 public class DefaultValuePlugin {
-    
+
     private static final Logger LOG = Logger.getLogger(DefaultValuePlugin.class.getName()); //NOPMD
+
+    // Known JAXB / JAXWS classes that do not have default constructors.
+    private static final Set<String> KNOWN_NO_DV_CLASSES = new HashSet<>(
+            Arrays.asList(
+                "jakarta.xml.ws.wsaddressing.W3CEndpointReference",
+                "jakarta.xml.bind.JAXBElement"
+            )
+        );
+
     private boolean complexTypes;
     private boolean active;
     
@@ -144,7 +156,7 @@ public class DefaultValuePlugin {
     private boolean isElementRequired(XSParticle particle) {
         return particle != null && getMinOccurs(particle) != 0 && getMaxOccurs(particle) == 1;
     }
-    
+
     private int getMinOccurs(XSParticle particle) {
         try {
             Number o = (Number)particle.getClass().getMethod("getMinOccurs").invoke(particle);
@@ -204,7 +216,7 @@ public class DefaultValuePlugin {
                     String varName = f.getPropertyInfo().getName(false);
                     JFieldVar var = co.implClass.fields().get(varName);
                     final JType rawType = f.getRawType();
-                    if (var != null && !"jakarta.xml.ws.wsaddressing.W3CEndpointReference".equals(rawType.fullName())) {
+                    if (var != null && !KNOWN_NO_DV_CLASSES.contains(rawType.erasure().fullName())) {
                         if (rawType instanceof JClass) {
                             final JClass jclazz = (JClass) rawType;
                             if (!jclazz.isAbstract() && !jclazz.isInterface()) {
